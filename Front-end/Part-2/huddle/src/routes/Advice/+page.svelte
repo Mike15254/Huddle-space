b002e2eb360b37167129382d5251004d98f957e9
Emergin <ngangaephantus112@gmail.com>
2023-03-14 22:05:24 +0300

    trial-two

--- Front-end/Part-2/huddle/src/routes/Advice/+page.svelte
+++ Front-end/Part-2/huddle/src/routes/Advice/+page.svelte
@@ -1,85 +1,176 @@
-<div id="title">
-	<h2>Advice</h2>
-</div>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"
-	><path
-		fill="#fffdd0"
-		fill-opacity="1"
-		d="M0,192L60,192C120,192,240,192,360,160C480,128,600,64,720,64C840,64,960,128,1080,154.7C1200,181,1320,171,1380,165.3L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
-	/></svg
->
-<center>
-	<div id="questions">
-		<div id="q1">
-			<h3>What advice is Tuzungumze recommending as you are looking for a therapist?</h3>
-			<p>
-				Tuzungumze is a Dekut based Student online Therapy platform: How it Can Help?   
+<main>
+	<div class="content-container">
+		<section class="hero-wrapper">
+			<div class="hero">
+				<div class="container">
+					<h3>What advice is Tuzungumze recommending as you are looking for a therapist?</h3>
+					<div class="typewritter">
+						<p>
+							Tuzungumze is a Dekut based Student online Therapy platform: How it Can Help? Are you
+							a student in Dekut who is feeling overwhelmed, anxious, or stressed out? If so,
+							Tuzungumze student therapy may be just what you need to get back on track. Online
+							student therapy is a form of therapy that is designed specifically for college
+							students and is conducted online. Some of the benefits of online student therapy
+							include increased accessibility, flexibility, and convenience. With Tuzungumze student
+							therapy, you can access mental health support when you need it, without having to
+							leave campus or schedule appointments during class time. This can be especially
+							helpful for students who live in remote areas, have busy schedules, or are
+							uncomfortable with traditional in-person therapy settings. Common mental health
+							challenges that campus students may face include stress, anxiety, depression, and
+							homesickness. Tuzungumze student therapy can help you address these challenges and
+							develop coping strategies that work for you. If you're interested in finding a
+							therapist, start by loging in Tuzungumze where we will connect you with the school's
+							counseling center and mental health services department. Be sure to read reviews and
+							ask for recommendations from friends or family members. Remember, it's okay to ask for
+							help when you need it. Seeking therapy is a sign of strength, not weakness. You are
+							not alone, and there are people who care about you and want to help. You've got this!
+						</p>
+					</div>
+					<nav class="nav-space1">
+						<blockquote5>
+							"The only way out is through."
+							<cite>- Robert Frost</cite>
+						</blockquote5><br /><br />
+						<blockquote0>
+							"Be the change that you wish to see in the world."
+							<cite>- Mahatma Gandhi</cite>
+						</blockquote0><br /><br />
+						<blockquote1>
+							"It does not matter how slowly you go as long as you do not stop."
+							<cite>- Confucius</cite>
+						</blockquote1><br />
+						<br />
+						<blockquote4>
+							"Life is 10% what happens to us and 90% how we react to it."
+							<cite>- Charles R. Swindoll</cite>
+						</blockquote4><br /><br />
+						<blockquote2>
+							"We cannot change the cards we are dealt, just how we play the hand."
+							<cite>- Randy Pausch</cite>
+						</blockquote2><br /><br />
+						<blockquote3>
+							"What lies behind us and what lies before us are tiny matters compared to what lies
+							within us."<br /><br />
+							<cite>- Ralph Waldo Emerson</cite>
+						</blockquote3><br />
+					</nav>
+				</div>
+			</div>
+		</section>
+	</div>
+</main>
 
-Are you a student in Dekut who is feeling overwhelmed, anxious, or stressed out? If so, Tuzungumze student therapy may be just what you need to get back on track. Online student therapy is a form of therapy that is designed specifically for college students and is conducted online.
-
-Some of the benefits of online student therapy include increased accessibility, flexibility, and convenience. With Tuzungumze student therapy, you can access mental health support when you need it, without having to leave campus or schedule appointments during class time. This can be especially helpful for students who live in remote areas, have busy schedules, or are uncomfortable with traditional in-person therapy settings.
-
-Common mental health challenges that campus students may face include stress, anxiety, depression, and homesickness. Tuzungumze student therapy can help you address these challenges and develop coping strategies that work for you.
-
-If you're interested in finding a therapist, start by loging in Tuzungumze where we will connect you with the school's counseling center and mental health services department. Be sure to read reviews and ask for recommendations from friends or family members.
-
-Remember, it's okay to ask for help when you need it. Seeking therapy is a sign of strength, not weakness. You are not alone, and there are people who care about you and want to help. You've got this!
-                
-			</p>
-            <div id="q2">
-                <h3>Here are some books you might find useful</h3>
-                <ul style="text-align: center;">
-                    <li>Feeling Good: The New Mood Therapy by David Burns</li>
-                    <li>The Anxiety and Phobia Workbook by Edmund Bourne</li>
-                    <li>The Power of Now: A Guide to Spiritual Enlightenment by Eckhart Tolle   This book teaches readers how to live in the present moment and find peace and happiness</li>
-                    <li>Maybe You Should Talk to Someone by Lori Gottlieb  </li>
-                    <li>The Mindful Way Through Depression" by Mark Williams, John Teasdale, Zindel Segal, and Jon Kabat-Zinn - This book provides readers with tools and techniques for using mindfulness to manage and overcome depression</li>
-                    <li>The Happiness Trap" by Russ Harris - This book teaches readers how to overcome common obstacles to happiness and develop a more fulfilling life.</li>
-                </ul>
-            </div> 
-            <div id="q3">
-                
-                <h2> The first step towards getting somewhere is to decide that you are not going to stay where you are." - John Pierpont Morgan </h2>
-                <h3> You don't have to see the whole staircase, just take the first step." - Martin Luther King Jr.</h3>
-                <h2> Happiness can be found, even in the darkest of times, if one only remembers to turn on the light." - Albus Dumbledore (from Harry Potter)</h2>
-                <h3> The greatest glory in living lies not in never falling, but in rising every time we fall." - Nelson Mandela</h3>
-            </div> 
-		</div>
-        </div>
-        
-</center>
-
-      
 <style>
-	#title {
+	blockquote0 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #1dd710;
+		padding: 10px;
+		text-align: left;
+		position: relative;
+	}
+	blockquote5 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #3113b6;
+		padding: 10px;
+		text-align: left;
+		position: left;
+	}
+	blockquote4 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #c6530b;
+		padding: 10px;
+		height: 0px;
+		text-align: left;
+		position: relative;
+	}
+	blockquote3 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #78aa0d;
+		padding: 10px;
+		text-align: left;
+	}
+	blockquote2 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #0c8dad;
+		padding: 10px;
+		text-align: left;
+	}
+	blockquote1 {
+		color: rgba(234, 240, 236, 0.85);
+		border-left: 5px solid #e61717;
+		background-color: #bc0d62;
+		padding: 10px;
+		text-align: left;
+	}
+
+	:root {
+		--hue: 223;
+		--bg: hsl(var(--hue), 90%, 90%);
+		--fg: hsl(var(--hue), 90%, 10%);
+		--trans-dur: 0.3s;
+		font-size: calc(16px + (20 - 16) * (100vw - 320px) / (1280 - 320));
+	}
+	.content-container {
+		background-size: cover;
+		background-image: linear-gradient(rgba(48, 5, 150, 0.85), rgb(51 83 67 / 85%)),
+			url(../therapy.jpg);
+		width: 110%;
+		height: 150vh;
+	}
+
+	.hero-wrapper {
 		text-align: center;
-		font-size: 32px;
-		line-height: 40px;
-		letter-spacing: -0.02em;
-		font-weight: 100;
-		background-color: #fffdd0;
-		height: 10vh;
-		padding: 30px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		height: 90vh;
 	}
-	#questions {
-		display: block;
-		background-color: #fff;
-		width: 100vh;
+
+	nav {
+		display: flex;
+		width: 100%;
+		justify-content: space-between;
+		cursor: pointer;
+		justify-self: unset;
 	}
-	#q1 {
-		border: 1px solid;
-		height: 20vh;
-		border-bottom-style: none;
-		border-right-style: none;
-		border-left-style: none;
-	}
-	#q1 h3 {
-		text-align: center;
-		font-size: 25px;
-	}
-	#q1 p {
-		text-align: center;
+
+	.nav-space1 {
+		display: inline-block;
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+		justify-content: left;
+		padding-top: 500px;
+		padding-left: 20px;
+		padding-right: 20px;
 		font-size: 20px;
+		text-align: center;
+		align-items: flex-start;
 	}
-    
+
+	.hero-wrapper .hero {
+		color: #ffffff;
+	}
+
+	h3 {
+		text-shadow: 1px 1px 1px red, 2px 2px 1px red;
+	}
+	p {
+		font-size: 0.7rem;
+		color: white;
+		line-height: 1.6;
+	}
+	.typewritter {
+		color: goldenrod;
+		padding: 10px;
+		padding-left: 20px;
+		justify-content: space-around;
+	}
 </style>
