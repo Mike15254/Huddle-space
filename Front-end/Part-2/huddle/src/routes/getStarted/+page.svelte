5fdf8099e1788ccd10a384b7e5de43f98b383698
Mike15254 <mike8max10@gmail.com>
2023-03-16 22:17:06 +0300

    Add progress tracker and navigation buttons to get started page

--- Front-end/Part-2/huddle/src/routes/getStarted/+page.svelte
+++ Front-end/Part-2/huddle/src/routes/getStarted/+page.svelte
@@ -15,10 +15,29 @@
 </div>
 
 <div class="greeting" style="display: {loading ? 'none' : 'block'}">
-	<h1>Hello there</h1>
+	<div class="container">
+		<h1>Welcome here</h1>
+		<div class="progress-container">
+			<div class="progress" id="progress" />
+			<div class="circle active">1</div>
+			<div class="circle">2</div>
+			<div class="circle">3</div>
+			<div class="circle">4</div>
+		</div>
+		<button class="btn" id="prev" disabled>Prev</button>
+		<button class="btn" id="next">Next</button>
+	</div>
 </div>
 
 <style>
+	* {
+		box-sizing: border-box;
+	}
+
+	:root {
+		--line-border-fill: #18b997;
+		--line-border-empty: #bdbdbd;
+	}
 	.loader {
 		margin: 200px auto;
 	}
@@ -38,16 +57,12 @@
 		position: absolute;
 		left: 50%;
 		margin-left: -10px;
-		-webkit-animation: 3s infinite linear;
-		-moz-animation: 3s infinite linear;
-		-o-animation: 3s infinite linear;
+		animation: 3s infinite linear;
 	}
 
 	.loader span:nth-child(2) {
 		background: #e84c3d;
-		-webkit-animation: kiri 1.2s infinite linear;
-		-moz-animation: kiri 1.2s infinite linear;
-		-o-animation: kiri 1.2s infinite linear;
+		animation: kiri 1.2s infinite linear;
 	}
 	.loader span:nth-child(3) {
 		background: #f1c40f;
@@ -55,12 +70,10 @@
 	}
 	.loader span:nth-child(4) {
 		background: #2fcc71;
-		-webkit-animation: kanan 1.2s infinite linear;
-		-moz-animation: kanan 1.2s infinite linear;
-		-o-animation: kanan 1.2s infinite linear;
+		animation: kanan 1.2s infinite linear;
 	}
 
-	@-webkit-keyframes kanan {
+	@keyframes kanan {
 		0% {
 			-webkit-transform: translateX(20px);
 		}
@@ -74,36 +87,8 @@
 			z-index: 200;
 		}
 	}
-	@-moz-keyframes kanan {
-		0% {
-			-moz-transform: translateX(20px);
-		}
 
-		50% {
-			-moz-transform: translateX(-20px);
-		}
-
-		100% {
-			-moz-transform: translateX(20px);
-			z-index: 200;
-		}
-	}
-	@-o-keyframes kanan {
-		0% {
-			-o-transform: translateX(20px);
-		}
-
-		50% {
-			-o-transform: translateX(-20px);
-		}
-
-		100% {
-			-o-transform: translateX(20px);
-			z-index: 200;
-		}
-	}
-
-	@-webkit-keyframes kiri {
+	@keyframes kiri {
 		0% {
 			-webkit-transform: translateX(-20px);
 			z-index: 200;
@@ -116,28 +101,88 @@
 		}
 	}
 
-	@-moz-keyframes kiri {
-		0% {
-			-moz-transform: translateX(-20px);
-			z-index: 200;
-		}
-		50% {
-			-moz-transform: translateX(20px);
-		}
-		100% {
-			-moz-transform: translateX(-20px);
-		}
+	h1 {
+		color: #212121;
+		margin-bottom: 90px;
 	}
-	@-o-keyframes kiri {
-		0% {
-			-o-transform: translateX(-20px);
-			z-index: 200;
-		}
-		50% {
-			-o-transform: translateX(20px);
-		}
-		100% {
-			-o-transform: translateX(-20px);
-		}
+
+	.container {
+		text-align: center;
+	}
+
+	.progress-container {
+		display: flex;
+		justify-content: space-between;
+		position: relative;
+		margin-bottom: 30px;
+		max-width: 100%;
+		width: 350px;
+	}
+
+	.progress-container::before {
+		content: '';
+		background-color: var(--line-border-empty);
+		position: absolute;
+		top: 50%;
+		left: 0;
+		transform: translateY(-50%);
+		height: 4px;
+		width: 100%;
+		z-index: -1;
+		transform: 0.4s ease;
+	}
+
+	.progress {
+		display: flex;
+		background-color: var(--line-border-fill);
+		position: absolute;
+		top: 50%;
+		left: 0;
+		transform: translateY(-50%);
+		height: 4px;
+		width: 0%;
+		z-index: -1;
+		transform: 0.4s ease;
+	}
+
+	.circle {
+		background-color: #ffffff;
+		color: #999;
+		border-radius: 50%;
+		height: 50px;
+		width: 50px;
+		border: 3px solid #b2bec3;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		transition: 0.4s ease;
+	}
+
+	.circle.active {
+		border-color: var(--line-border-fill);
+	}
+
+	.btn {
+		background-color: var(--line-border-fill);
+		color: #ffffff;
+		border-radius: 6px;
+		border: 0;
+		cursor: pointer;
+		padding: 8px 30px;
+		margin: 5px;
+		font-size: 14px;
+	}
+
+	.btn:focus {
+		outline: 0;
+	}
+
+	.btn:active {
+		transform: scale(0.97);
+	}
+
+	.btn:disabled {
+		background-color: var(--line-border-empty);
+		cursor: not-allowed;
 	}
 </style>
